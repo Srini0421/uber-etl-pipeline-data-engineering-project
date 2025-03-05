84630a4ba2e49383bba4c6af50d8736fd49e3de5
Srini0421 <srinivas.hosur07@gmail.com>
2025-03-06 02:06:36 +0530

    my Initial commit

--- mage-files/extract.py
+++ mage-files/extract.py
@@ -1,11 +1,11 @@
 import io
 import pandas as pd
 import requests
+
 if 'data_loader' not in globals():
     from mage_ai.data_preparation.decorators import data_loader
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
-
 
 @data_loader
 def load_data_from_api(*args, **kwargs):
@@ -16,7 +16,6 @@ def load_data_from_api(*args, **kwargs):
     response = requests.get(url)
 
     return pd.read_csv(io.StringIO(response.text), sep=',')
-
 
 @test
 def test_output(output, *args) -> None:
